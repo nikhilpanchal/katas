fcae1dea825982e4e196f404ce6979e2153724ff
nikhilpanchal <nikhilpanchal@gmail.com>
2017-02-23 15:59:40 -0500

    Completed the string math parser kata

--- stringmathparser/src/parser.js
+++ stringmathparser/src/parser.js
@@ -1,34 +1,78 @@
+var OP_RANK = {
+    '*': 0,
+    '/': 1,
+    '+': 2,
+    '-': 2
+}
 
-function calculate(str, operator) {
-    let operatorIndex = str.indexOf(operator)
+function calculateOperation(lOperand, rOperand, operator) {
+    // Potentially use the strategy pattern for this.
 
-    let operand1 = str.substr(0, operatorIndex)
-    let operand2 = str.substr(operatorIndex + 1)
-
+    // parseInt for lop and rop could have been done once been more dry.
     switch (operator) {
-        case "+": return parseInt(operand1, 10) + parseInt(operand2, 10)
-        case "*": return parseInt(operand1, 10) * parseInt(operand2, 10)
-        case "/": return parseInt(operand1, 10) / parseInt(operand2, 10)
-        case "-": return parseInt(operand1, 10) - parseInt(operand2, 10)
+        case "+":
+            return parseInt(lOperand, 10) + parseInt(rOperand, 10);
+        case "*":
+            return parseInt(lOperand, 10) * parseInt(rOperand, 10);
+        case "/":
+            return parseInt(lOperand, 10) / parseInt(rOperand, 10);
+        case "-":
+            return parseInt(lOperand, 10) - parseInt(rOperand, 10);
     }
 }
 
-// To be refactored
+function findOpIndex(str) {
+    let opIndex = [],
+        operators = ['+', '-', '*', '/'];;
+
+    for (let index in operators) {
+        opIndex.push(str.lastIndexOf(operators[index]));
+    }
+
+    // The index of the rightmost operator in str
+    return Math.max(...opIndex);
+}
+
+
+function isCurrentOpHigherThanPrevious(current, previous) {
+    return (OP_RANK[current] < OP_RANK[previous]);
+}
+
+function calculateRecursive(str) {
+    let opIndex = findOpIndex(str);
+    let lOperand, rOperand, operator;
+
+    // Single number expressions.
+    if (opIndex === -1) {
+        return parseInt(str, 10);
+    }
+
+    let previousOpIndex = findOpIndex(str.substr(0, opIndex));
+    // Multiple operation expressions.
+    if (previousOpIndex > 0 &&
+        isCurrentOpHigherThanPrevious(str.charAt(opIndex), str.charAt(previousOpIndex))) {
+        // the str.charAts could have been done earlier and saved in their own variables
+        // or in its own function, which would make the if much more concise.
+        let higherOrderOp = str.substr(previousOpIndex+1, opIndex);
+
+        rOperand = calculateRecursive(higherOrderOp);
+        opIndex = previousOpIndex;
+    } else {
+        // Single operation expressions or multi-ones where the previous op is not higher
+        // order than the current one.
+        rOperand = str.substr(opIndex+1);
+    }
+
+    operator = str.charAt(opIndex);
+    lOperand = calculateRecursive(str.substr(0, opIndex));
+
+
+    return calculateOperation(lOperand, rOperand, operator);
+}
+
 
 function parse(str) {
-    if(str.indexOf("+") > 0) {
-        return calculate(str, "+")
-    } else if(str.indexOf("*") > 0) {
-        return calculate(str, "*")
-    } else if(str.indexOf("-") > 0) {
-        return calculate(str, "-")
-    } else if(str.indexOf("/") > 0) {
-        return calculate(str, "/")
-    }
-
-    return parseInt(str, 10)
+    return calculateRecursive(str);
 }
 
-module.exports = {
-    parse
-}
+module.exports = parse;
