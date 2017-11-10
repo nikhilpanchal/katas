e3c686cfa610128cca2af45a83ddcc38d64f9428
nikhilpanchal <nikhilpanchal@gmail.com>
2017-11-10 07:40:40 -0500

    Iteration 4:
    Getting better. Forgot how to set the width of the bar this time. Getting a little more familiar with the select and scale apis.

--- d3-grouped-bar/src/charts/grouped-bar.js
+++ d3-grouped-bar/src/charts/grouped-bar.js
@@ -7,13 +7,13 @@ export default class GroupedBar {
 
         this.margin = {
             top: 20,
-            left: 20,
-            bottom: 20,
+            left: 40,
+            bottom: 30,
             right: 20
         };
 
-        this.graphHeight = height - this.margin.top - this.margin.bottom;
-        this.graphWidth = width - this.margin.left - this.margin.right;
+        this.chartHeight = height - this.margin.top - this.margin.bottom;
+        this.chartWidth = width - this.margin.left - this.margin.right;
 
         this.g = d3.select('.content')
             .append('svg')
@@ -21,16 +21,13 @@ export default class GroupedBar {
             .attr('width', width)
             .attr('class', 'chart')
             .append('g')
-            .attr('height', this.graphHeight)
-            .attr('width', this.graphWidth)
+            .attr('height', this.chartHeight)
+            .attr('width', this.chartWidth)
             .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
 
-        let barColors = ['red', 'blue', 'brown', 'green', 'orange', 'purple', 'grey'];
-
-        // Scales and Ranges
-        this.scaleX = d3.scaleBand().range([0, this.graphWidth]).paddingInner(0.1);
-        this.scaleY = d3.scaleLinear().range([this.graphHeight, 0]);
-        this.scaleBarColors = d3.scaleOrdinal().range(barColors);
+        this.scaleX = d3.scaleBand().range([0, this.chartWidth]).paddingInner(0.1);
+        this.scaleY = d3.scaleLinear().range([this.chartHeight, 0]);
+        this.scaleBarColor = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
     }
 
     render() {
@@ -41,27 +38,21 @@ export default class GroupedBar {
 
             return row;
         }, (error, data) => {
-            let dataColumns = data.columns.slice(1);
             let nameColumn = data.columns[0];
+            let dataColumns = data.columns.slice(1);
 
-            this.scaleX.domain(data.map((row) => {
-                return row[nameColumn];
-            }));
-
+            this.scaleX.domain(data.map((row) => row[nameColumn]));
             this.scaleBarGroup = d3.scaleBand()
                 .range([0, this.scaleX.bandwidth()])
                 .domain(dataColumns)
                 .paddingInner(0.05);
-
             this.scaleY.domain([0, d3.max(data, (row) => {
                 return d3.max(dataColumns, (col) => {
                     return row[col];
                 });
             })]);
+            this.scaleBarColor.domain(dataColumns);
 
-            this.scaleBarColors.domain(dataColumns);
-
-            // the action
             this.g.selectAll('g')
                 .data(data)
                 .enter()
@@ -82,11 +73,16 @@ export default class GroupedBar {
                 .append('rect')
                 .attr('x', (d) => this.scaleBarGroup(d.col))
                 .attr('y', (d) => this.scaleY(d.value))
-                .attr('height', (d) => this.graphHeight - this.scaleY(d.value))
+                .attr('height', (d) => this.chartHeight - this.scaleY(d.value))
                 .attr('width', (d) => this.scaleBarGroup.bandwidth())
-                .attr('fill', (d) => this.scaleBarColors(d.col));
+                .attr('fill', (d) => this.scaleBarColor(d.col));
 
-        })
+            this.g.append('g')
+                .attr('transform', `translate(0, ${this.chartHeight})`)
+                .call(d3.axisBottom(this.scaleX));
+            this.g.append('g')
+                .call(d3.axisLeft(this.scaleY).ticks(null, 's'));
+        });
 
     }
 }
