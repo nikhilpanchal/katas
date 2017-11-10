95b08dd6beb4e792efb8c9d7faaddeb3a2ae11b5
nikhilpanchal <nikhilpanchal@gmail.com>
2017-11-09 20:33:49 -0500

    Iteration 3:
    Coding from memory. You should look the api up if you're not sure how to use it.

--- d3-grouped-bar/src/charts/grouped-bar.js
+++ d3-grouped-bar/src/charts/grouped-bar.js
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
 
-class GroupedBar {
+export default class GroupedBar {
     constructor() {
         let height = 500;
         let width = 800;
@@ -12,54 +12,62 @@ class GroupedBar {
             right: 20
         };
 
-        let barColors = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"];
+        this.graphHeight = height - this.margin.top - this.margin.bottom;
+        this.graphWidth = width - this.margin.left - this.margin.right;
 
-        this.svg = d3.select('body')
+        this.g = d3.select('.content')
             .append('svg')
-            .attr('width', width)
             .attr('height', height)
-            .attr('class', 'chart');
+            .attr('width', width)
+            .attr('class', 'chart')
+            .append('g')
+            .attr('height', this.graphHeight)
+            .attr('width', this.graphWidth)
+            .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
 
-        this.containerHeight = height - this.margin.top - this.margin.bottom;
-        this.containerWidth = width - this.margin.left - this.margin.right;
+        let barColors = ['red', 'blue', 'brown', 'green', 'orange', 'purple', 'grey'];
 
-        this.scaleX = d3.scaleBand().range([0, this.containerWidth]);
-        this.scaleY = d3.scaleLinear().range([this.containerHeight, 0]);
-        this.scaleBarColor = d3.scaleOrdinal().range(barColors);
+        // Scales and Ranges
+        this.scaleX = d3.scaleBand().range([0, this.graphWidth]).paddingInner(0.1);
+        this.scaleY = d3.scaleLinear().range([this.graphHeight, 0]);
+        this.scaleBarColors = d3.scaleOrdinal().range(barColors);
     }
 
     render() {
-        d3.csv('/resources/age_group_data.csv', (data, index, columns) => {
-            columns.forEach((col, i) => {
-                if (i > 0) {
-                    data[col] = +data[col];
-                }
-            });
-            return data;
-        }, (data) => {
-            let g = this.svg.append('g')
-                .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
-                // .attr('height', this.containerHeight)
-                // .attr('width', this.containerWidth);
+        d3.csv('/resources/age_group_data.csv', (row, i, columns) => {
+            for (let i = 1; i < columns.length; i++) {
+                row[columns[i]] = +row[columns[i]];
+            }
 
+            return row;
+        }, (error, data) => {
             let dataColumns = data.columns.slice(1);
+            let nameColumn = data.columns[0];
 
-            this.scaleX.domain(data.map((row) => row[data.columns[0]]));
-            this.scaleGroup = d3.scaleBand().range([0, this.scaleX.bandwidth()]);
+            this.scaleX.domain(data.map((row) => {
+                return row[nameColumn];
+            }));
+
+            this.scaleBarGroup = d3.scaleBand()
+                .range([0, this.scaleX.bandwidth()])
+                .domain(dataColumns)
+                .paddingInner(0.05);
+
             this.scaleY.domain([0, d3.max(data, (row) => {
                 return d3.max(dataColumns, (col) => {
                     return row[col];
                 });
             })]);
-            this.scaleGroup.domain(dataColumns);
-            this.scaleBarColor.domain(dataColumns);
 
-            let ret = g.selectAll('g')
+            this.scaleBarColors.domain(dataColumns);
+
+            // the action
+            this.g.selectAll('g')
                 .data(data)
                 .enter()
                 .append('g')
                 .attr('transform', (row) => {
-                    return `translate(${this.scaleX(row[data.columns[0]])}, 0)`;
+                    return `translate(${this.scaleX(row[nameColumn])}, 0)`;
                 })
                 .selectAll('rect')
                 .data((row) => {
@@ -72,15 +80,13 @@ class GroupedBar {
                 })
                 .enter()
                 .append('rect')
-                .attr('x', (bar) => this.scaleGroup(bar.col))
-                .attr('y', (bar) => this.scaleY(bar.value))
-                .attr('height', (bar) => this.containerHeight - this.scaleY(bar.value))
-                .attr('width', this.scaleGroup.bandwidth())
-                .attr('fill', (bar) => this.scaleBarColor(bar.col));
+                .attr('x', (d) => this.scaleBarGroup(d.col))
+                .attr('y', (d) => this.scaleY(d.value))
+                .attr('height', (d) => this.graphHeight - this.scaleY(d.value))
+                .attr('width', (d) => this.scaleBarGroup.bandwidth())
+                .attr('fill', (d) => this.scaleBarColors(d.col));
 
-            console.log("Rendered a grouped bar graph");
-        });
+        })
+
     }
 }
-
-export default GroupedBar;
